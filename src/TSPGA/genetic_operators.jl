16cc72142175c9cb8b372a0359948145f987d0cb
aateg <aateg@live.com>
2023-03-09 15:36:01 +0100

    sem debugger nao sou ninguem

--- src/TSPGA/genetic_operators.jl
+++ src/TSPGA/genetic_operators.jl
@@ -3,9 +3,11 @@ module GeneticOperators
 export roulette_wheel_selection, tournament_selection, crossover, mutate!
 
 using Random: rand, AbstractRNG
-using StatsBase: sample!, Weights
+using StatsBase: sample, Weights
 
 Generation = Vector{Vector{Int}}
+
+# Selection -------------------------------------------------------------------
 
 function tournament_selection(generation::Generation, obj_function::Function, rng::AbstractRNG)
     fitness = obj_function.(generation)
@@ -19,36 +21,41 @@ end
 function tournament_selection!(idx_gen_parents::Vector{Int}, generation::Generation, obj_function::Function, rng::AbstractRNG)
     population_size = length(generation)
     idx_pop = sample(rng, 1:population_size, 2, replace=false)
-    fitness = obj_function.(generation[idx_pop])
-    push!(idx_gen_parents, idx_pop[argmax(fitness)]) 
+    fitness = [obj_function(generation[i]) for i in idx_pop]
+    push!(idx_gen_parents, idx_pop[argmax(fitness)])
 end
 
 function roulette_wheel_selection(generation::Generation, obj_function::Function, rng::AbstractRNG) 
     offspring_size = div(length(generation), 2)
-    fitness = obj_function.(generation)
+    fitness = [obj_function(chromosome) for chromosome in generation]
     idx_gen_parents = Int[]
     for _ in 1:offspring_size
-        roulette_wheel_selection!(idx_gen_parents, generation, obj_function, rng)
+        roulette_wheel_selection!(idx_gen_parents, generation, fitness, rng)
     end
     return idx_gen_parents
 end 
 
 function roulette_wheel_selection!(idx_gen_parents::Vector{Int}, generation::Generation, fitness::Vector{Float64}, rng::AbstractRNG)
     population_size = length(generation)
-    fitness = Weights(fitness ./ sum(fitness))
-    idx_parents = sample(rng, 1:population_size, fitness, 2, replace=false)
+    weights = Weights(fitness ./ sum(fitness))
+    idx_parents = sample(rng, 1:population_size, weights, 2, replace=false)
     push!(idx_gen_parents, idx_parents[1])
     push!(idx_gen_parents, idx_parents[2])
 end
 
-function crossover!(offspring::Generation, generation::Generation, p_cross::Float64, rng::AbstractRNG)
+# Mutation --------------------------------------------------------------------
+
+function crossover(idx_generation_parent::Vector{Int}, generation_parent::Generation, p_cross::Float64, rng::AbstractRNG)
     # get two on two combinations of chromosomes for population
     # and perform crossover
-    N = length(generation)
-    for i in 1:N, j in i+1:N
-        c1 = pmx_crossover(population[i], population[j], p_cross, rng)
+    N = length(idx_generation_parent)
+    offspring = Vector{Int}[]
+    for i in 1:2:N-1
+        j = i + 1
+        c1 = pmx_crossover(generation_parent[i], generation_parent[j], p_cross, rng)
         push!(offspring, c1)
     end
+    return offspring
 end
 
 function pmx_crossover(chromosome::Vector{Int}, other::Vector{Int}, maxcrosslen::Float64, rng::AbstractRNG)
@@ -75,6 +82,8 @@ function pmx_crossover(chromosome::Vector{Int}, other::Vector{Int}, maxcrosslen:
     end
     return new_chromosome
 end
+
+# Mutation --------------------------------------------------------------------
 
 function mutate!(generation::Generation, p_mutate::Float64, rng::AbstractRNG)
     for chromosome in generation
