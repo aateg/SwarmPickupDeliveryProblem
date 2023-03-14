c770367f0e966c637ab5a75e80f1a3eed20fe3be
Guilherme Fernandes <aateg@live.com>
2023-03-14 17:43:53 +0100

    Implementation of mutate (#7)
    * WIP: add mutate
    
    * fix: test mutate!

--- test/GeneticAlgorithm/Operators.jl
+++ test/GeneticAlgorithm/Operators.jl
@@ -1,7 +1,8 @@
-import SwarmPickupDeliveryProblem.GeneticAlgorithm.Operators
+using Random: MersenneTwister
+import SwarmPickupDeliveryProblem.GeneticAlgorithm.Operators: mutate!
 
 @testset "Test Operators" begin
-    
+    rng = MersenneTwister(1234)
     @testset "Selectors" begin
         @testset "Roulette Wheel Selection" begin
             nothing
@@ -17,7 +18,17 @@ import SwarmPickupDeliveryProblem.GeneticAlgorithm.Operators
     end
 
     @testset "Mutation" begin
-        nothing
+        @testset "Mutate Chromosome" begin
+            solution = [0, 1, 2, 3]
+            mutate!(solution, rng)
+            @test solution != [0, 1, 2, 3]
+        end
+        @testset "Mutate Generation" begin
+            generation = [[0, 1, 2, 3], [3, 2, 1, 0]]
+            pMutate = 1.0 # ensure mutation
+            mutate!(generation, pMutate, rng)
+            @test generation != [[0, 1, 2, 3], [3, 2, 1, 0]]
+        end
     end
 
 end
