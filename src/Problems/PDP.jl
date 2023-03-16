e330876a936e517c68533031f1bfb006fc9af3d8
aateg <aateg@live.com>
2023-03-16 19:11:00 +0100

    upgrade problem tests

--- src/Problems/PDP.jl
+++ src/Problems/PDP.jl
@@ -5,6 +5,8 @@ export PickupDeliveryProblem, generateRandomPDP, objFunction
 include("Utils.jl")
 import .Utils: DistanceMatrix, generateDistanceMatrix
 
+Solution = Vector{Int64}
+
 struct PickupDeliveryProblem
     numberOfPickupDeliveries::Int64
     distanceMatrix::DistanceMatrix
@@ -17,7 +19,7 @@ struct PickupDeliveryProblem
         P::Vector{Int64}, 
         D::Vector{Int64}
     )
-        new(numberOfPickupDeliveries, dist, P, D, nothing)
+        new(numberOfPickupDeliveries, dist, P, D)
     end
 end
 
@@ -30,7 +32,7 @@ function generateRandomPDP(numberOfPickupDeliveries::Int64, rng::AbstractRNG)
     return PickupDeliveryProblem(numberOfPickupDeliveries, distanceMatrix, P, D)
 end
 
-function totalDistancePairedPickupDelivery(x::Vector{Int64}, dist::Matrix{Float64}, P::Vector{Int64}, D::Vector{Int64})
+function totalDistancePairedPickupDelivery(x::Solution, dist::Matrix{Float64}, P::Vector{Int64}, D::Vector{Int64})
     s = 0
     for i in 1:length(x)-1
         s += dist[end, P[x[1]]] # depot to first pickup
@@ -41,7 +43,7 @@ function totalDistancePairedPickupDelivery(x::Vector{Int64}, dist::Matrix{Float6
     return s
 end
 
-function objFunction(x::Vector{Int64}, pdp::PickupDeliveryProblem)
+function objFunction(x::Solution, pdp::PickupDeliveryProblem)
     return 1 / totalDistancePairedPickupDelivery(x, pdp.distanceMatrix.d, pdp.P, pdp.D)
 end
 
--- test/Problems/Problems.jl
+++ test/Problems/Problems.jl
@@ -1,24 +1,77 @@
 using Random: MersenneTwister
 
-import SwarmPickupDeliveryProblem.Problems: generateRandomPDP, generateRandomTSP
+import SwarmPickupDeliveryProblem.Problems.TSP: objFunction, generateRandomTSP
+import SwarmPickupDeliveryProblem.Problems.PDP: objFunction, generateRandomPDP
+import SwarmPickupDeliveryProblem.Problems.Utils: generateDistanceMatrix, DistanceMatrix
 
 @testset "Test Problems" begin
     rng = MersenneTwister(1234)
 
+    @testset "Test Travel Salesman Problem" begin
+        numberOfCities = 3
+        tsp = generateRandomTSP(numberOfCities, rng)
     
-    @testset "Pickup Delivery Problem" begin
-        numberOfPickupDeliveries = 10
-        numberOfCities = 2 * numberOfPickupDeliveries + 1
-        pdp = generateRandomPDP(numberOfPickupDeliveries, rng)
-        possibleSolution = pdp.encoding
+        @testset "Test Random TSP initialization" begin
+            @test tsp.numberOfCities == numberOfCities
+            @test size(tsp.distanceMatrix) == (numberOfCities, numberOfCities)
+        end
+    
+        @testset "Test Objective Function Calculation" begin
+            possibleSolution = [1,2,3,4]
+            expectedTotalDistance = tsp.distanceMatrix.d[1,2] + tsp.distanceMatrix.d[2,3] \
+                + tsp.distanceMatrix.d[3,4] + tsp.distanceMatrix.d[4,1] 
+    
+            objective = objFunction(possibleSolution, tsp)
+    
+            @test 0 < objective < 1
+            @test objective ≈ 1/expectedTotalDistance atol=0.01
+        end
+    end
 
-        @test pdp.numberOfPickupDeliveries == numberOfPickupDeliveries
-        @test size(pdp.distanceMatrix) == (numberOfCities, numberOfCities)
-        @test size(pdp.X) == (numberOfCities,)
-        @test size(pdp.Y) == (numberOfCities,)
-        @test size(pdp.P) == (numberOfPickupDeliveries,)
-        @test size(pdp.D) == (numberOfPickupDeliveries,)
-        @test size(pdp.encoding) == (numberOfPickupDeliveries,)
-        @test 0 < pdp.objFunction(possibleSolution) < 1
+    @testset "Test Pickup Delivery Problem" begin
+        numberOfPickupDeliveries = 4
+        pdp = generateRandomPDP(numberOfPickupDeliveries, rng)
+
+        @testset "Test Random PDP initialization" begin
+            numberOfCities = 2 * numberOfPickupDeliveries + 1
+        
+            @test pdp.numberOfPickupDeliveries == numberOfPickupDeliveries
+            @test size(pdp.distanceMatrix) == (numberOfCities, numberOfCities)
+            @test size(pdp.P) == (numberOfPickupDeliveries,)
+            @test size(pdp.D) == (numberOfPickupDeliveries,)
+        end
+
+        @testset "Test Paired PDP Objective Function Calculation" begin
+            possibleSolution = [1,2,3,4]
+            expectedTotalDistance = \
+                pdp.distanceMatrix.d[end,P[1]] + \ # depot to first pickup
+                pdp.distanceMatrix.d[P[1],D[1]] + \ # pickup to delivery
+                pdp.distanceMatrix.d[D[1],P[2]] + \ # delivery to next pickup
+                pdp.distanceMatrix.d[P[2],D[2]] + \ # pickup to delivery
+                pdp.distanceMatrix.d[D[2],P[3]] + \ # delivery to next pickup
+                pdp.distanceMatrix.d[P[3],D[3]] + \ # pickup to delivery
+                pdp.distanceMatrix.d[D[3],P[4]] + \ # delivery to next pickup
+                pdp.distanceMatrix.d[D[4],end] # last delivery to depot
+    
+            objective = objFunction(possibleSolution, pdp)
+    
+            @test 0 < objective < 1
+            @test objective ≈ 1/expectedTotalDistance atol=0.01
+        end
+    end
+    
+    @testset "Test Utils" begin
+        @testset "Test Random Distance Matrix Generation" begin
+            N = 10
+            distanceMatrix = generateDistanceMatrix(N, rng)
+    
+            @test isa(distanceMatrix, DistanceMatrix)
+            @test size(distanceMatrix) == (N, N)
+            @test size(distanceMatrix.X) == (N,)
+            @test size(distanceMatrix.Y) == (N,)
+            for i in 1:N
+                @test distanceMatrix.d[i, i] == 0.0
+            end
+        end
     end
 end
