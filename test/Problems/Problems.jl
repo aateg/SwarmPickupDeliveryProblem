cecfb4f8b06d481bafc149e29d13ff399b87be83
aateg <aateg@live.com>
2023-03-17 17:14:59 +0100

    fix: test was wrong, forever mistery

--- test/Problems/Problems.jl
+++ test/Problems/Problems.jl
@@ -57,21 +57,20 @@ using SwarmPickupDeliveryProblem.Problems: TSP, PDP, Utils
         @testset "Test Paired PDP Objective Function Calculation" begin
             possibleSolution = [1, 2, 3, 4]
             expectedTotalDistance =
-                pdp.distanceMatrix.d[end, pdp.P[1]] \ # depot to first pickup
-                +pdp.distanceMatrix.d[pdp.P[1], pdp.D[1]] \ # pickup to delivery
-                +pdp.distanceMatrix.d[pdp.D[1], pdp.P[2]] \ # delivery to next pickup
-                +pdp.distanceMatrix.d[pdp.P[2], pdp.D[2]] \ # pickup to delivery
-                +pdp.distanceMatrix.d[pdp.D[2], pdp.P[3]] \ # delivery to next pickup
-                +pdp.distanceMatrix.d[pdp.P[3], pdp.D[3]] \ # pickup to delivery
-                +pdp.distanceMatrix.d[pdp.D[3], pdp.P[4]] \ # delivery to next pickup
-                +pdp.distanceMatrix.d[pdp.P[4], pdp.D[4]] \ # pickup to delivery
-                +pdp.distanceMatrix.d[pdp.D[4], end] # last delivery to depot
-            expectedObj = 1 / expectedTotalDistance
+                pdp.distanceMatrix.d[end, pdp.P[1]] + # depot to first pickup
+                pdp.distanceMatrix.d[pdp.P[1], pdp.D[1]] + # pickup to delivery
+                pdp.distanceMatrix.d[pdp.D[1], pdp.P[2]] + # delivery to next pickup
+                pdp.distanceMatrix.d[pdp.P[2], pdp.D[2]] + # pickup to delivery
+                pdp.distanceMatrix.d[pdp.D[2], pdp.P[3]] + # delivery to next pickup
+                pdp.distanceMatrix.d[pdp.P[3], pdp.D[3]] + # pickup to delivery
+                pdp.distanceMatrix.d[pdp.D[3], pdp.P[4]] + # delivery to next pickup
+                pdp.distanceMatrix.d[pdp.P[4], pdp.D[4]] + # pickup to delivery
+                pdp.distanceMatrix.d[pdp.D[4], end] # last delivery to depot
 
             objective = PDP.objFunction(possibleSolution, pdp)
 
             @test 0 < objective < 1
-            @test objective ≈ expectedObj atol = 0.01
+            @test objective ≈ 1/expectedTotalDistance atol = 0.01
         end
     end
 
