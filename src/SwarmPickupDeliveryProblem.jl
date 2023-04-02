eb5a3914d23d8048dbe99f386df9da5c494ae438
Guilherme Fernandes <aateg@live.com>
2023-04-02 19:38:08 +0200

    Heavy objects (#39)
    * formatting
    
    * WIP: almost there
    
    * WIP: heavy objects almost there
    
    * formatting
    
    * fix crossover stuff
    
    * formatting
    
    * [WIP] solve ordering problem
    
    * formatting and solve end missing
    
    * add more tests
    
    * fixing execution errors
    
    * improving logic of ordering
    
    * Solution Feasible working
    
    * formatting

--- src/SwarmPickupDeliveryProblem.jl
+++ src/SwarmPickupDeliveryProblem.jl
@@ -1,84 +1,149 @@
 module SwarmPickupDeliveryProblem
 
-export main
+export solveMPDPHeavyObjects, solutionContainsDuplicates, solutionCorrectlyOrdered
 
 using Random: MersenneTwister, randperm
 using StatsBase: sample
 using VRPGeneticAlgorithm: Chromosome, Parameters, geneticAlgorithm
 
-include("pickupDeliveryProblem.jl")
+include("mPDP.jl")
 
-function initializeGeneration(
-    numberOfPickupDeliveries::Int64,
-    numberOfVehicles::Int64,
-    populationSize::Int64,
-    rng::AbstractRNG,
-)
-    return collect(
-        Chromosome(
-            randperm(rng, numberOfPickupDeliveries), # requests
-            sample(rng, 1:numberOfVehicles, numberOfPickupDeliveries, replace = true), # vehicles
-        ) for i = 1:populationSize
-    )
-end
-
-function printSolution(solution::Chromosome, problem::Problem)
+function printSolution(solution::Chromosome, requestsWeights, problem::Problem)
     # for each vehicle I want to know the cities it visits
     # and compute the total distance
     println("\n")
-    println("Chromosome:")
-    println("- Chromosome: ", solution.requests)
-    println("- Vehicles: ", solution.vehicles)
+    println(
+        "Solution With Duplicates? ",
+        solutionContainsDuplicates(solution.requests, solution.vehicles),
+    )
+    println("R:", solution.requests)
+    println("Q:", [requestsWeights[i] for i in solution.requests])
+    println("V:", solution.vehicles)
     cost = objFunction(solution.requests, solution.vehicles, problem)
     println("- Total Distance: ", 1 / cost)
     println("Cost: ", cost)
     println("Routes:")
     for vehicle = 1:problem.numberOfVehicles
         visits = solution.requests[solution.vehicles.==vehicle]
-        s = Int64[]
-        push!(s, 0)
-        for visit in visits
-            push!(s, problem.P[visit])
-            push!(s, problem.D[visit])
-        end
-        push!(s, 0)
-        println("- Vehicle $vehicle: ", s)
+        println("- Vehicle $vehicle: ", [0; visits; 0])
     end
 end
 
-function main()
+function solutionFeasible(solution::Chromosome, problem::Problem)
+    solutionWithDuplicates =
+        solutionContainsDuplicates(solution.requests, solution.vehicles)
+    realizable = solutionCorrectlyOrdered(
+        solution.requests,
+        solution.vehicles,
+        [problem.requestsWeights[i] for i in solution.requests],
+    )
+    return !solutionWithDuplicates && realizable
+end
+
+function solutionContainsDuplicates(R::Vector{Int64}, V::Vector{Int64})
+    N = length(R)
+    for i = 1:N
+        for j = i+1:N
+            if R[i] == R[j] && V[i] == V[j]
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function isVehicleAllocated(v::Int64, allocations::Dict{Int64,Vector{Int64}})
+    for (_, value) in allocations
+        if v in value
+            return true
+        end
+    end
+    return false
+end
+
+function solutionCorrectlyOrdered(R::Vector{Int64}, V::Vector{Int64}, Q::Vector{Int64})
+    # this function assumed that there are (R_i, V_i) duplicates
+    # then all vehicles associated with R_i are different
+    N = length(R)
+    allocations = Dict{Int64,Vector{Int64}}() # mapping requests and allocated vehicles
+    for i = 1:N
+        if isVehicleAllocated(V[i], allocations)
+            # cannot send an allocated vehicle to another request
+            return false
+        else
+            # Able to be allocated
+            if R[i] in keys(allocations)
+                push!(allocations[R[i]], V[i])
+            else
+                allocations[R[i]] = [V[i]]
+            end
+        end
+        if Q[R[i]] == length(allocations[R[i]])
+            # max vehicles allocated
+            # clear allocations
+            allocations[R[i]] = Int64[]
+        end
+    end
+    return true
+end
+
+function initializeGenerationHeavyObjects(
+    requestsWeights::Vector{Int64},
+    numberOfVehicles::Int64,
+    populationSize::Int64,
+    rng::AbstractRNG,
+)
+    requests = [r for r = 1:length(requestsWeights) for _ = 1:requestsWeights[r]]
+    N = length(requests)
+    return collect(
+        Chromosome(
+            shuffle(rng, requests), # requests
+            sample(rng, 1:numberOfVehicles, N, replace = true), # vehicles
+        ) for i = 1:populationSize
+    )
+end
+
+function solveMPDPHeavyObjects(nRequests::Int64, nVehicles::Int64, maxWeight::Int64)
     # Random Number Generator
     rng = MersenneTwister(1234)
 
     # Problem Definition
-    numberOfPickupDeliveries = 10
-    numberOfVehicles = 4
-    problem = generateRandomMPDP(numberOfPickupDeliveries, numberOfVehicles, rng)
+    problem = generateRandomHeavyObjectProblem(nRequests, nVehicles, maxWeight, rng)
     printProblem(problem)
 
     # Genetic Algorithm Parameters
-    parameters = Parameters(10, 100, 0.8, 0.2)
+    parameters = Parameters(100, 100, 0.8, 0.6)
 
     # Initialization
-    generationParent = initializeGeneration(
-        problem.numberOfPickupDeliveries,
+    generationParent = initializeGenerationHeavyObjects(
+        problem.requestsWeights,
         problem.numberOfVehicles,
         parameters.populationSize,
         rng,
     )
 
     # Redefine Objective function
-    fitnessFunction(solution::Chromosome) =
-        objFunction(solution.requests, solution.vehicles, problem)
+    function fitnessFunction(solution::Chromosome)
+        if solutionFeasible(solution, problem)
+            return objFunction(solution.requests, solution.vehicles, problem)
+        end
+        return 1E-8
+    end
 
-    # Execution
-    generationParent =
-        geneticAlgorithm(generationParent, fitnessFunction, parameters, false, rng)
+    # Execution 
+    generationParent = geneticAlgorithm(
+        generationParent,
+        fitnessFunction,
+        parameters,
+        rng,
+        true,
+        problem.requestsWeights,
+    )
 
     # Output
-    printSolution(generationParent[1], problem)
-    printSolution(generationParent[end], problem)
-
+    printSolution(generationParent[1], problem.requestsWeights, problem)
 end
+
+solveMPDPHeavyObjects(5, 3, 2)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,63 @@
 using Test
+using SwarmPickupDeliveryProblem
 
 @testset "Test SwarmPickupDeliveryProblem" begin
-    nothing
+    @testset "Check Duplicates" begin
+        @test solutionContainsDuplicates([1, 1, 1], [1, 1, 1]) == true
+        @test solutionContainsDuplicates([1, 1, 2], [1, 1, 1]) == true
+        @test solutionContainsDuplicates([2, 1, 1], [2, 2, 2]) == true
+        @test solutionContainsDuplicates([1, 2, 3], [1, 2, 3]) == false
+        @test solutionContainsDuplicates([1, 2, 3], [1, 1, 1]) == false
+        @test solutionContainsDuplicates([1, 1, 3], [1, 2, 1]) == false
+    end
+
+
+    @testset "Check Solution Ordering" begin
+        @testset "Single vehicle problem" begin
+            R = [1, 2, 3]
+            Q = [1, 1, 1]
+            V = [1, 1, 1]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == true
+        end
+
+        @testset "Unit Capacity problem" begin
+            R = [1, 2, 3]
+            Q = [1, 1, 1]
+            V = [1, 2, 3]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == true
+        end
+
+        @testset "Allocated vehicles cannot go to another request" begin
+            R = [1, 2, 1]
+            Q = [2, 1, 2]
+            V = [1, 1, 2]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == false
+        end
+        @testset "Wrong git testing" begin
+            R = [3, 4, 8, 5, 2, 5, 3, 1, 9, 6, 2, 7, 4, 1, 7, 10]
+            Q = [2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 1]
+            V = [1, 2, 3, 1, 3, 3, 2, 3, 2, 3, 2, 2, 1, 2, 3, 1]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == false
+        end
+
+        @testset "Possible Solution" begin
+            R = [1, 1, 2]
+            Q = [2, 2, 1]
+            V = [1, 2, 1]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == true
+        end
+
+        @testset "Example of delaying request 1" begin
+            R = [1, 2, 2, 1]
+            Q = [2, 2, 2, 2]
+            V = [1, 2, 3, 2]
+
+            @test solutionCorrectlyOrdered(R, V, Q) == true
+        end
+    end
 end
