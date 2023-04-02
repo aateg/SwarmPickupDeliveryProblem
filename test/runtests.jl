10ace0bfdd69cb3497605d55941a7153231ea9ae
aateg <aateg@live.com>
2023-04-02 14:46:06 +0200

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,29 +14,33 @@ using SwarmPickupDeliveryProblem
 
     @testset "Check Solution Ordering" begin
         @testset "Problem with only unit capacities" begin
-            R = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-            V = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            requestsWeights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            R = [1, 2, 3]
+            Q = [1, 1, 1]
+            V = [1, 1, 1]
 
-            @test checkOrdering(R, V, requestsWeights) == true
+            @test checkOrdering(R, V, Q) == true
         end
         @testset "Problem that sends allocated vehicle to another request" begin
-            R = [3, 4, 8, 5, 2, 5, 3, 1, 9, 6, 2, 7, 4, 1, 7, 10]
-            Q = [2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 1]
-            V = [1, 2, 3, 1, 3, 3, 2, 3, 2, 3, 2, 2, 1, 2, 3, 1]
+            R = [1, 2, 1]
+            Q = [2, 1, 2]
+            V = [1, 1, 2]
 
-            @test checkOrdering(R, V, requestsWeights) == false
+            @test checkOrdering(R, V, Q) == false
         end
         @testset "Problem that changes from heavy request before current request it's finished" begin
             R = [3, 4, 8, 5, 2, 5, 3, 1, 9, 6, 2, 7, 4, 1, 7, 10]
             Q = [2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 1]
             V = [1, 2, 3, 1, 3, 3, 2, 3, 2, 3, 2, 2, 1, 2, 3, 1]
 
-            @test checkOrdering(R, V, requestsWeights) == false
+            @test checkOrdering(R, V, Q) == false
         end
 
         @testset "Possible Solution" begin
-            nothing
+            R = [3, 3, 8]
+            Q = [2, 2, 1]
+            V = [1, 2, 1]
+
+            @test checkOrdering(R, V, Q) == true
         end
     end
 end
